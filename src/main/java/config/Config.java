7a488145f4827460c630c7ae4899769662b279f0
Qaden <qaden86@gmail.com>
2025-12-11 13:09:11 +0200

    Updated BaseTest and other files

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,7 +1,7 @@
 package config;
 
 public class Config {
-    public static final String BASE_URL = "http://localhost:3000";
+    public static final String BASE_URL = "https://pyavchik.space";
     public static final String LOGIN = "test1";
     public static final String PASSWORD = System.getenv("TEST_PASSWORD");
 }
--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
 import components.ModalComponent;
 import config.Config;
 
@@ -22,10 +24,10 @@ public class MainPage {
     }
 
     public void clickAddCard() {
-        page.getByRole(
-                AriaRole.BUTTON,
-                new Page.GetByRoleOptions().setName("+ New Card")
-        ).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("+ New Card"))
+                .waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
+
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("+ New Card")).click();
     }
 
     public ModalComponent modal() {
