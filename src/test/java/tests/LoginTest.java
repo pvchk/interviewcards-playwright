47eff5b5f6c0814973e990523322ba2730bc2a17
Oleksander Pyavchik <pyavchik@gmai.com>
2025-12-17 21:02:43 +0200

    added allure report screenshots

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.interviewcards.BaseTest;
 import config.Config;
+import io.qameta.allure.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import pages.LoginPage;
@@ -10,10 +11,15 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Epic("Authentication")
+@Feature("Login")
 public class LoginTest extends BaseTest {
 
-    @ParameterizedTest(name = "Login with identifier: {0}")
+    @Story("User should be able to login with valid credentials")
+    @Description("Test verifies that user can successfully login using email or username")
+    @Severity(SeverityLevel.CRITICAL)
     @MethodSource("validLoginData")
+    @ParameterizedTest(name = "Login with identifier: {0}")
     void loginWithValidCredentials(String login) {
         loginPage = new LoginPage(page);
         mainPage = loginPage.login(login, Config.PASSWORD);
